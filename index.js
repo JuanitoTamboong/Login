eeee493da76be845380e9c66c72b2c13979cdf5c
Juanito Tamboong <93064994+JuanitoTamboong@users.noreply.github.com>
2022-09-21 15:06:38 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+ function validate()
+        {
+           var username = document.getElementById('username').value;
+           var password = document.getElementById('password').value;
+           if(username == "juanito" && password == "tamboong"){
+           alert("Login Succesfully");
+           window.location = "https://iq-pips.netlify.app/";
+               return true;
+           }
+         
+           else if(username !== "IQpips" && password !== "Block1"){
+               alert("wrong username or password");
+               return false;
+            }
+        }
+        function toggle(){
+            var ShowPass = document.getElementById('password');
+            if(ShowPass.type === "password"){
+            ShowPass.type = "text";
+            }
+            else{
+                ShowPass.type = "password"
+            }
+        }
